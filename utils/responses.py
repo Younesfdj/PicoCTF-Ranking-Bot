36fd59d96cb71733dac6f85cbbcd7a7cfa5e477a
Younesfdj <ferradjiyounes69@gmail.com>
2024-06-14 00:19:32 +0100

    feat: added leaders to the leaderboard

--- utils/responses.py
+++ utils/responses.py
@@ -1,4 +1,4 @@
-from utils.leaderboard import extractLeaderboard, getUserRank
+from utils.leaderboard import extractLeaderboard, getUserRank, orderLeaderboard
 from utils.message_formatters import leaderboard_formatter, user_rank_formatter
 from dotenv import load_dotenv
 from utils.api_request import getLeaderboard
@@ -6,12 +6,16 @@ import os
 
 load_dotenv()
 DATA_FILE: str = os.getenv('DATA_FILE_PATH')
+LEADERS_CLASSROOM_URL: str = os.getenv('LEADERS_CLASSROOM_URL')
 
 def generate_response(user_message:str,csvFile=DATA_FILE)->str:
     input_text = user_message.split("/picoCTF")[1].strip().split()
     leaderboard = getLeaderboard()
     if leaderboard == -1:
         leaderboard = extractLeaderboard(csvFile)
+    leaders_leaderboard = getLeaderboard(LEADERS_CLASSROOM_URL)
+    if leaders_leaderboard != -1:
+        leaderboard = orderLeaderboard([y for x in [leaderboard, leaders_leaderboard] for y in x])
     if "leaderboard" in input_text: 
         return leaderboard_formatter(leaderboard)
     elif "rank" in input_text:
